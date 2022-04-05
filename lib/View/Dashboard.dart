f5018a6397af2b49408f2958f4f0b62f71016084
hemko2020 <dihuer.hermann@icloud.com>
2022-04-05 11:18:01 +0200

    Update V3

--- lib/View/Dashboard.dart
+++ lib/View/Dashboard.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../Models/UserModel.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -9,12 +16,32 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  List<UserModel> userFile = [];
+
+  ///////////////// FETCH DATA USER API///////////////////////////
+
+  static const String urlApi = 'https://api.github.com/users/defunkt';
+
+  Future<List<UserModel>> getUser() async {
+    final response = await http.get(Uri.parse(urlApi));
+    var data = json.decode(response.body);
+    if (response.statusCode == 200) {
+      for (Map json in data) {
+        userFile.add(UserModel.fromJson(data));
+      }
+      return userFile;
+    } else {
+      return userFile;
+    }
+  }
+
+/////////////////////////////// BUILD DASHBOARD///////////////////////////
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
-        title: Text(
+        title: const Text(
           "Github Dashboard",
           style: TextStyle(
             fontSize: 25,
@@ -24,12 +51,40 @@ class _DashboardPageState extends State<DashboardPage> {
       ),
       body: SafeArea(
           child: Container(
-        padding: EdgeInsets.all(15),
+        padding: const EdgeInsets.all(25),
         child: Column(
           children: [
+            //////////////// SEARCH BAR ///////////////////////////
+
             CupertinoSearchTextField(
               borderRadius: BorderRadius.circular(20),
+              autocorrect: false,
+              padding: const EdgeInsets.all(10),
             ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            ///////////////// BUILD REPO LIST//////////////////////
+
+            FutureBuilder(
+                future: getUser(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator(
+                      color: Colors.deepPurple,
+                    );
+                  } else {
+                    return ListView.builder(
+                        itemCount: userFile.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: const EdgeInsets.all(10),
+                          );
+                        });
+                  }
+                })
           ],
         ),
       )),
